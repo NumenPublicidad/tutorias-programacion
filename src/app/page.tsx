fe9aa19efd5a4a3eb6198623a6c643869817edde
rosmildoLopes <[200~123316625+rosmildoLopes@users.noreply.github.com~>
2023-11-24 13:06:47 -0300

    Actualizar otra vez el responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { tutoriasData } from './constants/constants';
 
 export default function Home(): any {
 	return (
-		<main className='grid  md:grid-cols-2 gap-8 text-black object-contain sm:w-full mt-5 sm:mx-auto md:w-full md:mt-16 lg:w-[80%] '>
+		<main className='grid  md:grid-cols-2 sm:grid-cols-1 gap-8 text-black object-contain sm:w-full mt-5 sm:mx-auto md:w-full md:mt-16 lg:w-[80%] '>
 			{tutoriasData.map(tutoria => (
 				<Tutorias key={tutoria.id} tutoria={tutoria} />
 			))}
