4c6e9a802804a2de14abf93c5882fdb5203faf19
Tomas Panza <tomasjosuep@hotmail.com>
2023-11-30 16:28:36 -0300

    Agregar datos

--- src/app/clasesEspeciales/ClasesEspeciales.tsx
+++ src/app/clasesEspeciales/ClasesEspeciales.tsx
@@ -2,16 +2,18 @@ import { TfiAlarmClock } from 'react-icons/tfi';
 
 export const ClasesEspeciales = ({ ClasesEspeciales }) => {
 	return (
-		<div className='flex flex-col justify-between border border-gray-300 rounded-md shadow-md'>
-			<div className='flex flex-col w-full items-center'>
-				<h2 className='text-xl w-full text-center font-bold bg-black text-white p-3 rounded-t-lg'>
+		<div className='flex flex-col h-full justify-between rounded-lg shadow-md'>
+			<div className='ml: flex flex-col h-28 w-full place-content-center items-center bg-white rounded-t-lg p-3'>
+				<h2 className='text-xl w-full text-center font-bold  text-black'>
 					{ClasesEspeciales.name}
 				</h2>
-				<h3 className='text-lg mb-2 text-white'>
+			</div>
+			<div className='h-1/2 border rounded-b-lg'>
+				{/* <h3 className='text-lg mb-2 text-white'>
 					{ClasesEspeciales.day}
-				</h3>
+				</h3> */}
 				<h3
-					className='text-lg mb-2 font-bold text-indigo-500 items-center'
+					className='pt-2 text-lg mb-2 font-bold text-indigo-500 items-center'
 					style={{
 						display: 'flex',
 						justifyContent: 'center',
@@ -21,19 +23,21 @@ export const ClasesEspeciales = ({ ClasesEspeciales }) => {
 					{ClasesEspeciales.hour}
 				</h3>
 
-				<h3 className='text-lg mb-2 text-indigo-400'>
+				{/* <h3 className='text-lg mb-2 text-indigo-400'>
 					{`Profesor: ${ClasesEspeciales.teacher}`}
-				</h3>
-			</div>
-			<div className='flex justify-center'>
-				<a
-					href={ClasesEspeciales.zoom}
-					className='text-white p-3'
-				>
-					<button className='bg-blue-500 md:px-3 md:py-2 px-3 py-1 lg:px-3 rounded-xl hover:bg-white hover:text-blue-600 hover:font-bold'>
-						Enlace de la reunión
-					</button>
-				</a>
+				</h3> */}
+
+				<div className='flex justify-center'>
+					<a
+						target={'_blank'}
+						href={ClasesEspeciales.zoom}
+						className='text-white p-3'
+					>
+						<button className='bg-blue-500 md:px-3 md:py-2 px-3 py-1 lg:px-3 rounded-xl hover:bg-white hover:text-blue-600 hover:font-bold'>
+							Enlace de la clase
+						</button>
+					</a>
+				</div>
 			</div>
 		</div>
 	);
